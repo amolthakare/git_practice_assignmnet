b8e35f3970abbc6885f78e11960c92535a2ab626
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2022-09-19 09:24:38 +0530

    updated code

--- problem1.js
+++ problem1.js
@@ -2,13 +2,13 @@ var num=3;
 
 var count=0;
     
-    for(var i=2;i<=num;i++){
-        if(num%i==0){
+    for(var i=2;i<num;i++){
+        if(num%i===0){
             count++;
         }
     }
     
-    if(count>1){
+    if(count!==0){
         console.log("No");
     }
     else{
--- problem2.js
+++ problem2.js
@@ -1,10 +1,10 @@
 
-var str="werg";
+var str="madam";
 var N=str.length;
 var reverse="";
   for(var i=N-1;i>=0;i--){
       reverse+=str[i];
-    //   console.log(i);
+    
   }
   if(str==reverse){
       console.log("Yes");
